5b32fdf15a635c4c470b74f30a36588a51846545
Anna Szmyga <anna.szmyga@leocode.com>
2022-01-27 15:52:20 +0100

    Add steps section - 1 step

--- js/script.js
+++ js/script.js
@@ -1,8 +1,40 @@
 const profitTiles = document.querySelectorAll('.profitTile');
-console.log(profitTiles);
 
 profitTiles.forEach(tile => {
     tile.addEventListener('click', () => {
         tile.classList.toggle('profitTile-active');
     })
-})
+});
+
+const rangeInput = document.querySelector('.slider');
+const rangeOutput = document.querySelector('.sliderOutput');
+const price = document.querySelector('.price');
+
+const prices = {
+    0: 0,
+    1: 100,
+    2: 200,
+    3: 300,
+    4: 400,
+    5: 500,
+    6: 600,
+    7: 700,
+    8: 800,
+    9: 900,
+    10: 1000,
+}
+
+price.innerText = `${prices[5]}zł`;
+
+function handleInputChange(e) {
+    const min = e.target.min
+    const max = e.target.max
+    const val = e.target.value
+
+    rangeOutput.style.left = (val - min) * 100 / (max - min) + '%'
+    e.target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+    price.innerText = `${prices[e.target.value]}zł`;
+
+}
+
+rangeInput.addEventListener('input', handleInputChange);
